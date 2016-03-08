944436f9ead63af6a86eb1d736c1fe0750d96ee6
endikap100 <ealdecoa004@ikasle.ehu.es>
2016-03-09 00:23:05 +0100

    download trends download tweets of trend and get the location

--- TwitterLocation.py
+++ TwitterLocation.py
@@ -6,6 +6,20 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.secure = True
 api = tweepy.API(auth)
 
-#print api.trends_available()
-print api.trends_place(1)
+#1 for global geolocation
+global_tendencies = api.trends_place(1)
+tendencies = []
+#print api.geo_id(07002)
 
+for trend in global_tendencies[0]["trends"]:
+    tendencies.append(trend["name"])
+
+#print tendencies
+
+print tendencies[0]
+
+for tweet in tweepy.Cursor(api.search, q=(tendencies[0])).items():
+    if tweet.coordinates != None:
+        print ""
+        print tweet
+        print tweet.coordinates
