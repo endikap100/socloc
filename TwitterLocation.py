d6ddfe114619d2a3959bfbb86d9deb11ee51d294
endika <ealdecoa004@ikasle.ehu.es>
2016-04-05 17:10:34 +0200

    update db conection

--- TwitterLocation.py
+++ TwitterLocation.py
@@ -9,7 +9,6 @@ cursor = cnx.cursor()
 consumer_key, consumer_secret = [line.rstrip('\n') for line in open('twitterKeys.txt')]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret,"http://soclocapi.appspot.com/callback")
-#auth.set_access_token("471159702-nDvOvrkwvSdLQDCj2RZy94LUweMfXh1XhoSXuKyT", "bgL3Q2yYS6nAYGj7jzDqEmYd8mtZUT7kLBM2t0YlXHuJC")
 auth.secure = True
 api = tweepy.API(auth)
 #print api
@@ -32,7 +31,7 @@ for tweet in tweepy.Cursor(api.search, q=(tendencies[0])).items():
         print tweet.coordinates
         #location = Location(hashtag=tendencies[0], location=tweet.coordinates)
         #location.put()
-        add_location = ("INSERT INTO location(hashtag, location)VALUES (%s, %s, %s, %s, %s)")
+        add_location = ("INSERT INTO location(hashtag, location)VALUES (%s, %s)")
         data_location = (tendencies[0], tweet.coordinates)
 
         # Insert new employee
